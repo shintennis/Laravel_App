9d92018369393fca0b86bd55bff78a36bcb67565
shinsaku <shin.tennis.1126@gmail.com>
2020-03-15 03:40:20 +0900

    EC2へ

--- laravel_app/resources/views/navbar.blade.php
+++ laravel_app/resources/views/navbar.blade.php
@@ -32,14 +32,10 @@
                                 <li>
                                     <a class="px-4 mr-2 btn btn-outline-success" href="{{ url('create') }}">投稿</a>
                                
-                                    <a class="mr-2  btn btn-outline-dark" href="/user/{{ Auth::user()->id }}"><i class="far fa-user" style="font-size: 20px; color: gray;"></i></a>
+                                    <a class="mr-2  btn btn-outline-info" href="/user/{{ Auth::user()->id }}"><i class="far fa-user" style="font-size: 20px;"></i></a>
                                 
-                                    <a class="btn btn-outline-info" href="{{ route('logout') }}">
-                                        ログアウト
-                                    </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
+                                    <a class="btn btn-outline-dark common-btn edit-profile-btn" rel="nofollow" data-method="POST" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
                                 </li>
                               
                             
